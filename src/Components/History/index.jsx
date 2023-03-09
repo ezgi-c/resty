c2c20f5aab45a6c36543a6e914ddd91d64a10dcc
EZGI COBAN <SARAHEZGICOBAN@GMAIL.COM>
2023-03-08 21:15:01 -0800

    History component renders previous api calls, unable to render the data

---
+++ src/Components/History/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function History({history}) {  
+    return (
+      <div>
+        <h2>History</h2>
+        <ul>
+          {history.map((apiCall, index) => (
+            <li key={index} 
+            // onClick={() => handleHistoryClick(index)}
+            >
+              <div>Method: {apiCall.method}</div>
+              <div>URL: {apiCall.url}</div>
+              {/* <div>DATA: {apiCall.data}</div> */}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default History;
